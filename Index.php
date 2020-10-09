99401a6d806fc2b3d1d5208e52609eb66b45a22e
Jean-Sawano <sawano.projetos@gmail.com>
2020-10-09 17:57:35 -0300

    criando index php

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="mit" content="2020-09-04T20:57:32-03:00+32430">
+    <meta name="viewport" content="width-device-width,initial-scale-1">
+
+    <title>FSPHP: Iniciando um Projeto!</title>
+    <link rel="Stylesheet" href="assets/style.css">
+
+</head>
+
+<body>
+</body>
+
+</html>
